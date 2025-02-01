87fa4e2b05fc39789298315b45e25e34dc120b7e
frsswq <farissaifuddin.work@gmail.com>
2025-02-02 00:11:33 +0700

    style: update navbar styling

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,13 +1,10 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
+  plugins: [
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-classnames",
+    "prettier-plugin-merge",
   ],
 };
