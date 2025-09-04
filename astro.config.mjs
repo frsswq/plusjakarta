325e91fd91b943bad94f10e2f6158267d81e9f8e
frsswq <farissaifuddin.work@gmail.com>
2025-09-04 19:22:33 +0700

    fix: remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import vercelStatic from '@astrojs/vercel/static';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	output: 'static',
-	adapter: vercelStatic({
-		webAnalytics: {
-			enabled: true
-		},
-		maxDuration: 8
-	}),
-
 	vite: {
 		plugins: [tailwindcss()],
 		css: {
